faf56ab56abec32a32290db612ebdc2e0cc0f158
HMSBennett <harrisonsbennett@gmail.com>
2019-07-08 13:57:16 +0000

    attempt One

---
+++ meanInstall.sh
@@ -0,0 +1,34 @@
+sudo apt-get update
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+sudo apt-get install git
+
+git clone https://github.com/Nboaram/TeamAPoolProjectBackend.git
+
+git clone https://github.com/Nboaram/TeamAPoolProje
+ctUI.git
+
+cd TeamAPoolProjectBackend/
+
+git checkout Developer
+
+npm install
+
+cd 
+
+cd TeamAPoolProjectUI/
+
+git checkout Developer
+
+npm install
+
+sudo npm install -g @angular/cli
+
+sudo apt install -y mongodb
+
+sudo systemctl start mongodb
+
+
