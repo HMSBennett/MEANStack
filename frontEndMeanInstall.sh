7176005bb9db019cb3159dc77ea5703cdc2dd930
HMSBennett <harrisonsbennett@gmail.com>
2019-07-10 10:45:25 +0100

    front end tidy up

--- frontEndMeanInstall.sh
+++ frontEndMeanInstall.sh
@@ -1,13 +1,25 @@
-cd
-
-git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
-
-cd TeamAPoolProjectUI/
-
-git checkout Developer
-
-npm install
+sudo apt-get update
 
 sudo npm install -y -g @angular/cli
 
-ng serve --host 0.0.0.0
+sudo cp frontend.service /etc/systemd/system
+
+sudo useradd --create-home angular
+
+sudo usermod -shell /bin/bash angular
+
+sudo su - angular -c "
+
+git clone https://github.com/Nboaram/TeamAPoolProjectUI.git &&
+
+cd TeamAPoolProjectUI/ &&
+
+git checkout Developer &&
+
+npm install &&
+
+sudo systemctl daemon-reload &&
+
+sudo systemctl start frontend &&
+
+exit"
