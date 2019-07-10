b95cd8e3276d14b6d1947ada8e88326e5e582d53
HMSBennett <harrisonsbennett@gmail.com>
2019-07-10 10:51:05 +0100

    npm update

--- frontEndMeanInstall.sh
+++ frontEndMeanInstall.sh
@@ -1,7 +1,5 @@
 sudo apt-get update
 
-sudo npm install -y -g @angular/cli
-
 sudo cp frontend.service /etc/systemd/system
 
 sudo useradd --create-home angular
@@ -10,13 +8,15 @@ sudo usermod -shell /bin/bash angular
 
 sudo su - angular -c "
 
-git clone https://github.com/Nboaram/TeamAPoolProjectUI.git &&
+git clone https://github.com/Nboaram/TeamAPoolProjectUI &&
 
 cd TeamAPoolProjectUI/ &&
 
 git checkout Developer &&
 
 npm install &&
+
+sudo npm install -y -g @angular/cli &&
 
 sudo systemctl daemon-reload &&
 
