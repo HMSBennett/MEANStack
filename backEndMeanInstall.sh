1c2bc1cc80a95f598f69eca520dc0466a8f4a4e2
HMSBennett <harrisonsbennett@gmail.com>
2019-07-10 09:24:10 +0100

    Update on service files

--- backEndMeanInstall.sh
+++ backEndMeanInstall.sh
@@ -6,8 +6,6 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
 sudo apt-get install -y nodejs
 
-sudo apt-get install git
-
 git clone https://github.com/Nboaram/TeamAPoolProjectBackend.git
 
 cd TeamAPoolProjectBackend/
@@ -16,18 +14,20 @@ git checkout Developer
 
 npm install
 
-sudo useradd --create-home mongodbadm
+sudo useradd --create-home mongodbAdm
 
-sudo usermod --shell /bin/bash mongodbadm
+sudo usermod --shell /bin/bash mongodbAdm
 
-sudo su - mongodbadm
+sudo su - mongodbAdm
 
 sudo apt install -y mongodb
 
-sudo cp mongodbadm.service ~/etc/systemd/system
+cd
+
+cd MEANStack/
+
+sudo cp backend.service /etc/systemd/system
 
 sudo systemctl daemon-reload
 
-sudo systemctl start mongodb
-
-node Index.js
+sudo systemctl start backend
