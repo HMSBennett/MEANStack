f505315707dac2444861b4518be7d7f5b0bf169e
Harrison Bennett <32133987+HMSBennett@users.noreply.github.com>
2019-07-12 10:12:55 +0100

    Update mongoInstall.sh

--- mongoInstall.sh
+++ mongoInstall.sh
@@ -8,6 +8,12 @@ sudo mkdir $HOME/db
 
 sudo mongod --dbpath $HOME/db --port 8080 --fork --logpath /var/tmp/mongodb
 
+#sed -i "11d" /etc/mongodb.conf
+
+#echo "bind_ip = 0.0.0.0" >> /etc/mongodb.conf
+
+mongod --bind_ip localhost,0.0.0.0
+
 sudo systemctl daemon-reload
 
 sudo systemctl start mongodb
