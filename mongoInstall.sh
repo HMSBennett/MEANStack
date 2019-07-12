d0869d7536c3917b1d458e3146beb646d40e0ba3
Harrison Bennett <32133987+HMSBennett@users.noreply.github.com>
2019-07-12 15:24:59 +0100

    Update mongoInstall.sh

--- mongoInstall.sh
+++ mongoInstall.sh
@@ -1,3 +1,7 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+
 sudo apt-get update
 
 sudo apt install -y mongodb
@@ -6,7 +10,7 @@ sudo systemctl stop mongodb
 
 sudo rm /lib/systemd/system/mongodb.service
 
-sudo cp mongodb.service /lib/systemd/system
+sudo cp mongodb.service /etc/systemd/system
 
 sudo systemctl daemon-reload
 
